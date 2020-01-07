bb28e7ec82f4be0c458d478dd4c6caec304afd04
Shady-Data <rechunn@gmail.com>
2020-01-07 10:45:59 -0500

    added demo_lab_2b.c and perf_lab_2.c

---
+++ studentcode/demo_lab_2b.c
@@ -0,0 +1,37 @@
+/*
+* 
+*  name: Robert Chunn
+*  date: 06 Jan 2020
+*  project: Demo Lab 2
+*
+*  URL: https://github.com/CyberTrainingUSAF/08-C-Programming/blob/master/02_Variables/07_Type_conversions.md
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int integer = 44;
+    float singlePrecision = 2.2;
+    double doublePrecision = 3.1415926535897932;
+    char singleChar = '$';
+    char singleChar2 = 33;
+
+    // printf("\n\n\nsize of integer is %ld \n", sizeof(integer));
+    printf("\n\n\nValue of integer is %d \n", integer);
+    printf("Value of integer as a float is %f \n", (float)integer);
+    printf("Value of integer as a char is %c \n\n", (char)integer);
+    // printf("size of float is %ld \n", sizeof(singlePrecision));
+    printf("Value of float is %f \n", singlePrecision);
+    printf("Value of float as a double is %f \n", (double)singlePrecision);
+    // printf("size of double is %ld \n", sizeof(doublePrecision));
+    printf("Value of double is %f \n", doublePrecision);
+    printf("Value of double as a float is %f \n\n", (float)doublePrecision);
+    // printf("size of char 1 is %ld \n", sizeof(singleChar));
+    printf("Value of char 1 is %c \n", singleChar);
+    printf("Value of char 1 as an integer is %d \n", (int)singleChar);
+    // printf("size of char 2 is %ld \n\n\n", sizeof(singleChar2));
+    printf("Value of %d as a char is %c\n", 33, (char)33);
+
+    return 0;
+}
