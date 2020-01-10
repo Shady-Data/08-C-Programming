d51081bb66dc34df64c6c37d3ae30ef57229f12e
Shady-Data <rechunn@gmail.com>
2020-01-10 15:15:35 -0500

    refactored output of perf_lab_10.c

--- studentcode/perf_lab_10.c
+++ studentcode/perf_lab_10.c
@@ -21,19 +21,25 @@ int main(void)
     // move the 1 set bit in bitChecker to the left most bit in the 32-bit unsigned integer
     bitChecker <<= 31;
 
+    // declare and initialize a spacer accumalator
+    int spacer = 0;
     // while bitchecker is greater than 0 
     while (bitChecker > 0)
     {
-        // Check if bitchecker is the start of a new 4 bit grouping
-        if (bitChecker == 8 | bitChecker  == 128 | bitChecker == 2048 | bitChecker == 32768 | bitChecker == 524288 | bitChecker == 8388608 | bitChecker == 134217728)
+        // if spacer has accumalated to 4
+        if ( spacer == 4)
         {
             // print a space a specific bit places to create 4 bit groupings
             fprintf(stdout, " ");
+            // reset the spacer to 0
+            spacer = 0;
         }
         // print 1 (true) if the result of userinput AND bitchecker is > 0 or 0 (False) if not
         fprintf(stdout, "%d", (userInput & bitChecker) > 0);
         // move the bitchecker one bit to the right
         bitChecker >>= 1;
+        // add 1 to spacer at the end of each loop
+        spacer++;
     }
     // print a newline for readability
     fprintf(stdout, "\n");
