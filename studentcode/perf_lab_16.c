ebafec811fc7227327dfd61503893846a3677871
Shady-Data <rechunn@gmail.com>
2020-01-13 16:00:54 -0500

    Added perf_lab_16.c

---
+++ studentcode/perf_lab_16.c
@@ -0,0 +1,42 @@
+/* 
+* 
+*  name: Robert Chunn
+*  date: 13 Jan 2020
+*  project: Performance Lab 16
+*
+*  URL: https://github.com/CyberTrainingUSAF/08-C-Programming/blob/master/07_Control_flow/performance_labs/Lab16.md
+*
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // declare and initialize a char Array containing multiple phrases
+    char phrases[] = "I hate individually assigning array elements.\nWe're questioning how to accomplish the lab.\nHow do we accomplish this task?\n\0";
+    // test the phrase with puts
+    puts(phrases);
+
+    // add 2 newlines to seperate the output
+    fprintf(stdout, "\n\n");
+
+    // declare and initialize an int variable counter for reading individual chars from the phrases char array and a newline counter
+    int ind = 0, newline = 0;
+    // while the char at index position is not a NUL '\0' char
+    while (phrases[ind] != '\0')
+    {
+        if (newline < 1)
+        {
+            putchar(phrases[ind]); // print the current char
+        }
+        if (phrases[ind] == '\n')
+        {
+            newline++;
+        }
+        ind++; // increment the index
+    }
+    fprintf(stdout, "\n");
+    
+    return 0;
+}
