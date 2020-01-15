786707e375a1b35b0568bb1ce0dcb73653cbcf2c
Shady-Data <rechunn@gmail.com>
2020-01-15 12:37:55 -0500

    refactored myhead.h and added myhead.c

--- studentcode/myhead.h
+++ studentcode/myhead.h
@@ -1,29 +1,6 @@
 /* This file contains useful function declarations for myself */
 
-#include <stdio.h>
+#include "myhead.c"
 
-void print_binary(int num)
-{
-    __uint32_t bitVar = 0x01;
-    bitVar <<= 31;
-
-    int space = 0;
-    while (bitVar > 0)
-    {
-        if (space == 4)
-        {
-            fprintf(stdout, " ");
-            space = 0;
-        }
-        fprintf(stdout, "%d", (num & bitVar) > 0);
-        bitVar >>= 1;
-        space++;
-    }
-    fprintf(stdout, "\n");
-}
-
-void clear_input()
-{
-    int ch;
-    while ((ch = getchar()) != '\n' && ch != EOF);
-}
+extern void print_binary(int num);
+extern void clear_input();
---
+++ studentcode/testheader.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myhead.h"
+
+void main()
+{
+    print_binary(65535);
+    printf("\n");
+}
