7c6b35462f8e51499b6f63c3f37be95513da32f7
Shady-Data <rechunn@gmail.com>
2020-01-23 10:39:59 -0500

    Refactored perf_lab_27.c

--- studentcode/perf_lab_27.c
+++ studentcode/perf_lab_27.c
@@ -9,15 +9,20 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ERROR_NULL_POINTER "Error! Null Pointer!"
-#define ERROR_ABUNDANT_POINTER "Error! Too many delimiters found!"
-#define ERROR_NULL_DELIMITER "Error! NULL delimiter"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define RED_ERROR ANSI_COLOR_RED "ERROR! " ANSI_COLOR_RESET
+
+#define ERROR_NULL_POINTER "NULL Pointer/Empty String found!"
+#define ERROR_ABUNDANT_POINTER "More than 1 occurence of the delimiter character found!"
+#define ERROR_NULL_DELIMITER "NULL character/No Character passed for the delimiter split value!"
 
 char * split_the_string(char * string_ptr, char delimiter)
 {
     int delim_count = 0;
     char * delim_ptr = string_ptr;
-    if (*string_ptr == '\0' || string_ptr == NULL){
+    if (*string_ptr == '\0' || (int *)string_ptr == NULL){
         return ERROR_NULL_POINTER;
     }
 
@@ -29,7 +34,7 @@ char * split_the_string(char * string_ptr, char delimiter)
         if (*string_ptr == delimiter){
             if (delim_count == 0){
                 delim_count++;
-                delim_ptr = string_ptr;
+                delim_ptr = string_ptr + 1;
             }
             else{
                 return ERROR_ABUNDANT_POINTER;
@@ -38,12 +43,7 @@ char * split_the_string(char * string_ptr, char delimiter)
         string_ptr++;
     }
     
-    if (delim_count == 0){
-        return delim_ptr;
-    }
-    else{
-        return delim_ptr + 1;
-    }
+    return delim_ptr;
 }
 
 int main(void)
@@ -52,27 +52,46 @@ int main(void)
     char my_string2[] = "Lets give this a ,try with, two commas!";
     char my_string3[] = "Lets give this a try with no commas!";
     char my_string4[] = "Lets give this a \0try with a NULL char!";
-    for (int i = 0; i < 4; i++){
-        switch (i)
-        {
-        case 0:
-            printf("%s\n", split_the_string(my_string1, ','));
-            break;
-        case 1:
-            printf("%s\n", split_the_string(my_string2, ','));
-            break;
-        case 2:
-            printf("%s\n", split_the_string(my_string3, ','));
-            break;
-        case 3:
-            printf("%s\n", split_the_string(my_string4, '\0'));
-            break;
-
-        default:
-        printf("Loop exceeded expected cases.\n");
-            break;
+    char my_string5[] = "";
+    char * multiStringArrays[] = {my_string1, my_string2, my_string3, my_string5};
+    char * tempValue_ptr;
+    for (int i = 0; i < sizeof(multiStringArrays)/sizeof(*multiStringArrays); i++){
+        tempValue_ptr = split_the_string(multiStringArrays[i], ',');
+        if (tempValue_ptr == ERROR_NULL_POINTER || tempValue_ptr == ERROR_ABUNDANT_POINTER || tempValue_ptr == ERROR_NULL_DELIMITER){
+            printf(RED_ERROR "%s\n", tempValue_ptr);
         }
-    }
+        else{
+            printf("%s\n", tempValue_ptr);
+        }
+        // switch (i)
+        // {
+        // case 0:
+        //     printf("%s\n", split_the_string(my_string1, ','));
+        //     break;
+        // case 1:
+        //     printf("%s\n", split_the_string(my_string2, ','));
+        //     break;
+        // case 2:
+        //     printf("%s\n", split_the_string(my_string3, ','));
+        //     break;
+        // case 3:
+        //     printf("%s\n", split_the_string(my_string4, '\0'));
+        //     break;
+        // case 4:
+        //     printf("%s\n", split_the_string(my_string5, ','));
+        //     break;
 
+        // default:
+        // printf("Loop exceeded expected cases.\n");
+        //     break;
+        // }
+    }
+    tempValue_ptr = split_the_string(my_string4, '\0');
+    if (tempValue_ptr == ERROR_NULL_POINTER || tempValue_ptr == ERROR_ABUNDANT_POINTER || tempValue_ptr == ERROR_NULL_DELIMITER){
+        printf(RED_ERROR "%s\n", tempValue_ptr);
+    }
+    else{
+        printf("%s\n", tempValue_ptr);
+    }
     return 0;
 }
