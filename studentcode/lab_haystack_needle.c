8d383ffb90de3637540093d0b9758163d3b3f340
Shady-Data <rechunn@gmail.com>
2020-02-10 10:20:54 -0500

    completed lab_haystack_needle.c

--- studentcode/lab_haystack_needle.c
+++ studentcode/lab_haystack_needle.c
@@ -1,8 +1,3 @@
-|[Table of Contents](/00-Table-of-Contents.md)|
-|---|
-
----
-
 /*
 Haystack Needle Lab
 
@@ -12,9 +7,10 @@ AKA: Move the substring from the string using memory operators and functions
 
 */
 
-```c
-
 // TODO: Include any needed headers
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int haystack_needle(char *haystack, char *needle, char *buffer);
 int main()
@@ -23,12 +19,18 @@ int main()
   char substring[] = " brand new";
   // TODO: Create a char buffer named *buffer using malloc \
   ensure it is large enough to hold string[] + a nul-terminator
-
+  char * buffer = malloc((strlen(string) + 1) * sizeof(char));
   // TODO: Ensure buffer was created
+  if (buffer == NULL){
+    puts("Memory Allocation failed. Exitting");
+    exit(1);
+  }
   haystack_needle(string, substring, buffer);
 
   // TODO: Give back the memory from buffer
+  free(buffer);
   // TODO: Print out the modified string
+  puts(string);
 
   return 0;
 
@@ -56,19 +58,15 @@ int haystack_needle(char *haystack, char *needle, char *buffer)
     // TODO: Using strstr() find the needle in the haystack (substring in string)\
     the pointer that is returned is pointing to the substring within the string \
     Set that pointer to a new char pointer called *mark_position
+    char * mark_position = strstr(haystack, needle);
 
     // TODO: Using strcpy(), store the needle into the buffer... \
     you are not authorized to use the variable needle.
+    strcpy(buffer, needle);
 
     // TODO: Using memmove, overwrite the haystack (string)... effectivly removing the needle \
     // HINT: start at the mark_position... that is after all where we need to start replacing.
+    memmove(mark_position, mark_position + needleLength, needleLength);
 
     return 0;
 }
-
-```
-
----
-
-|[Next Topic](/14_Structs/README.md)|
-|---|
